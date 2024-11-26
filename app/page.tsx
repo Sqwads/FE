ea8bc64777f4523623730906f27e2bb9736b8121
HamidTech <hammedowolabi2001@gmail.com>
2024-11-26 19:56:38 +0100

    fixing image issue

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Value from "@/components/home/Value";
 export default function Home() {
   return (
     <>
-      <div className=" hero-bg min-h-screen py-4 lg:py-7 px-2 md:px-5 lg:px-14">
+      <div className=" hero-bg min-h-screen py-4 lg:py-7 px-2 md:px-5 lg:px-14" style={{backgroundImage: 'url("/images/Sqwads-bg.svg")'}}>
         <Navbar />
         <Hero />
       </div>
