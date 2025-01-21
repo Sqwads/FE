18c0c0e815dd2dadc28575e2bb7a6f1ff7e5dcba
HamidTech <hammedowolabi2001@gmail.com>
2025-01-21 20:41:33 +0100

    allowing http on backend

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ const nextConfig = {
   reactStrictMode:false,
   experimental: {
       missingSuspenseWithCSRBailout: false,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Match API requests
+        destination: 'http://16.171.155.101:3000/api/:path*', // Proxy to your backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
