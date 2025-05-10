c3125360fc303f25ae2e3c9974ad4b55f652b518
HamidTech <hammedowolabi2001@gmail.com>
2025-05-10 18:08:04 +0100

    imlementing the user dashboard page

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,8 @@ const nextConfig = {
       {
         source: '/api/:path*', // Match API requests
         // destination: 'http://16.171.155.101:3000/api/:path*', // Proxy to your backend
-        destination:'http://localhost:4000/api/:path*',
-        // destination:'https://sqwads-be.vercel.app/api/:path*',
+        // destination:'http://localhost:4000/api/:path*',
+        destination:'https://sqwads-be.vercel.app/api/:path*',
       },
     ];
   },
