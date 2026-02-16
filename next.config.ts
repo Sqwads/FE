e84d5d1dee65be477b2e7d34cfe91b9a70bbc282
HamidTech <hammedowolabi2001@gmail.com>
2026-02-16 14:36:20 +0100

    fixing the prompt for profile completion

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: false, // User requested no fetching logs
+    },
+  },
   experimental: {
     // missingSuspenseWithCSRBailout: false,
     optimizePackageImports: [
